fbf6b351450b554750756802351ad67787357977
Maxime Raguénès <llyffeur@gmail.com>
2021-09-07 12:22:20 +0200

    UPDATE sortie | registration(twig)

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,6 +39,11 @@ class SortieController extends AbstractController
      */
     public function detailsSortie(int $id): Response
     {
-        return $this->render('sortie/sortie.html.twig');
+
+       // $repo = $this->getDoctrine()->getRepository(Sortie::class);
+       // $sortie = $repo->find($id);
+        return $this->render('sortie/sortie.html.twig' //, [ 'sortie' => $sortie
+   //     ]
+    );
     }
 }
