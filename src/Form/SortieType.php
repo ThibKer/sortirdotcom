f9ac2fce8918227dd3d9838f0e8cc45727f2c1b7
Thibaut Kerouedan <thibaut.kerouedan2021@campus-eni.fr>
2021-09-08 16:37:56 +0200

    UPDATE => resolution erreur formulaire sortieCreation / token manquant

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -7,6 +7,8 @@ use App\Entity\Site;
 use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +19,7 @@ class SortieType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('dateHeureDebut')
+            ->add('dateHeureDebut', DateTimeType::class)
             ->add('duree')
             ->add('nbInscriptionMax')
             ->add('infosSortie')
@@ -26,9 +28,6 @@ class SortieType extends AbstractType
             ->add('site', EntityType::class, [
                 "class" => Site::class,
                 "choice_label" => "nom"
-            ])
-            ->add('create', SubmitType::class, [
-                "label" => "Créer"
             ])
         ;
     }
