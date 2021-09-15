fbbf18930e5f00e57a894bdf3b7a56aaf0137bbf
Romuald29 <romuald.gagnon2021@campus-eni.fr>
2021-09-15 14:41:30 +0200

    UPDATE css

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -147,8 +147,10 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
             'Mieko',
             'Toshie',
             'Junko');
+        $surnames = array("Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler");
 
         shuffle($names);
+        shuffle($surnames);
         for ($i = 0; $i < 20; $i++) {
             $product = new Participant();
             $site1=$this->getReference(SiteFixtures::SITE_REFERENCE1);
@@ -159,9 +161,9 @@ class AppFixtures extends Fixture implements DependentFixtureInterface
                 case 1 : $site=$array[1];break;
             }
             $product->setPseudo($names[$i]);
-            $product->setNom($names[$i]);
+            $product->setNom($surnames[$i]);
             $product->setPrenom($names[$i]);
-            $product->setEmail($names[$i] . "@mail.fr");
+            $product->setEmail($names[$i].".".$surnames[$i] . "@mail.fr");
             $product->setPassword($this->passwordEncoder->encodePassword($product,"123456"));
             $product->setTelephone("01234567" . $i);
             $product->setSite($site);
